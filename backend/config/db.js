64d08a36ab637ff12e9adb00a689e63bdda54ac7
Abdul Wahid Sharifi <abdulwahid.sharifi@nresult.com>
2025-07-21 11:25:49 +0430

    Completed Project

---
+++ backend/config/db.js
@@ -0,0 +1,14 @@
+import { neon } from "@neondatabase/serverless";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { PGHOST, PGDATABASE, PGUSER, PGPASSWORD } = process.env;
+
+// creates a SQL connection using our env variables
+export const sql = neon(
+  `postgresql://${PGUSER}:${PGPASSWORD}@${PGHOST}/${PGDATABASE}?sslmode=require`
+);
+// this sql function we export is used as a tagged template literal, which allows us to write SQL queries safely
+
+// postgresql://neondb_owner:npg_NW8olSOGfx5E@ep-dawn-meadow-a83i0d05-pooler.eastus2.azure.neon.tech/neondb?sslmode=require
